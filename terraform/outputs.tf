3cc1e5a9b9ac0f88657f6a072dbfd06b6eac7148
Oiane <oruizmo@gmail.com>
2023-03-19 16:23:52 +0100

    Devolvemos IP pública para conexión con VM

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,4 +2,10 @@ output "resource_group_id" {
   value = azurerm_resource_group.rg.id
 }
 
+output "vm_id" {
+  value = azurerm_linux_virtual_machine.vm.id
+}
 
+output "vm_pip" {
+  value = azurerm_linux_virtual_machine.vm.public_ip_address
+}
