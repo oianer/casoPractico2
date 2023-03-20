6423a5756859b6cc0ec54dcb97095a5f3a9d936e
Oiane <oruizmo@gmail.com>
2023-03-20 12:49:08 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,7 +11,12 @@ output "vm_pip" {
   value = azurerm_linux_virtual_machine.vm.public_ip_address
 }
 
-output "acr_pass" {
-  value = azurerm_container_registry.acr1.admin_password
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
   sensitive = true
 }
