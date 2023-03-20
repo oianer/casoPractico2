d491fc145449be09ff12efb6670add1e437c246e
Oiane <oruizmo@gmail.com>
2023-03-20 10:23:08 +0100

    Se comenta el código

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,8 +1,10 @@
+# Creación del resource group en el que estará desplegado el proyecto
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location_name
 }
 
+# Creación de la red virtual de la máquina virtual que crearemos más adelante
 resource "azurerm_virtual_network" "vnet" {
   name                = var.network_name
   address_space       = ["10.0.0.0/16"]
@@ -10,6 +12,7 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+# Creación de la subred dentro de la red virtual anterior
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
@@ -17,6 +20,7 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+# Creación de la IP pública de la máquina virtual, que será estática y en el tier Standard
 resource "azurerm_public_ip" "pip" {
   name                = "VIP"
   location            = azurerm_resource_group.rg.location
@@ -25,6 +29,7 @@ resource "azurerm_public_ip" "pip" {
   sku                 = "Standard"
 }
 
+# Creación de la interfaz de red que utilizará la máquna virtual. Se le asigna como ip pública la ip estática anterior
 resource "azurerm_network_interface" "nic" {
   name                = "vnic"
   location            = azurerm_resource_group.rg.location
@@ -34,10 +39,11 @@ resource "azurerm_network_interface" "nic" {
     name                          = "externa"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-	public_ip_address_id          = azurerm_public_ip.pip.id
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
+# Creación de grupo de seguridad de red para admitir conexiones de entrada SSH
 resource "azurerm_network_security_group" "nsg1" {
   name                = "securitygroup"
   location            = azurerm_resource_group.rg.location
@@ -56,7 +62,7 @@ resource "azurerm_network_security_group" "nsg1" {
   }
 }
 
-
+# Se crea una asociación de grupos de seguridad de red con el grupo anterior
 resource "azurerm_network_interface_security_group_association" "nisga" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg1.id
@@ -67,6 +73,7 @@ resource "azurerm_subnet_network_security_group_association" "nsg-link" {
   network_security_group_id = azurerm_network_security_group.nsg1.id
 }
 
+# Se crea una regla de seguridad para permitir las conexiones http en el puerto 8080 y se incluye en el grupo de seguridad anterior
 resource "azurerm_network_security_rule" "http" {
   name		      = "http"
   priority	      = 1002
@@ -81,11 +88,12 @@ resource "azurerm_network_security_rule" "http" {
   network_security_group_name = azurerm_network_security_group.nsg1.name
 }	
 
+# Se crea la máquina virtual "oruizmo" con sistema operativo Ubuntu 22_04, con la interfaz de red definida anteriormente, 
+# la clave pública ssh del equipo en el que corre, y un disco estándar
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "vm1"
+  name                = "oruizmo"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-#  size                = "Standard_F2"
   size                = "Standard_F2s_v2"	
   admin_username      = "azureuser"
   network_interface_ids = [
