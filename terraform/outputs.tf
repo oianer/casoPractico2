8b5f92cbc7f71eaf484616659cb1a7053d55205d
Oiane <oruizmo@gmail.com>
2023-03-20 15:41:06 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,11 +12,11 @@ output "vm_pip" {
 }
 
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  value     = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
   sensitive = true
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
+  value = azurerm_kubernetes_cluster.k8s.kube_config_raw
   sensitive = true
 }
